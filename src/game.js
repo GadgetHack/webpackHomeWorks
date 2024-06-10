5b1e436ec9c7ae18fcf6033ee875d81ca181cc6b
Tatiana Afonin <tatiana-afonina@gmail.com>
2024-06-10 21:20:13 +0200

    webpackHomeWork

---
+++ src/game.js
@@ -0,0 +1,17 @@
+// src/game.js
+import Character from './domain';
+
+export default class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
